88eba52a949af12b174a0de2051b778a66cae78d
riccardo <r.iervolino93@gmail.com>
2020-09-25 21:40:24 +0200

    huge commit

---
+++ Jebudo_Dice/Assets/Scripts/Dice.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    AudioSource [] audioSource;
+    Rigidbody dice;
+    int count = 0;
+    // Start is called before the first frame update
+    public GameObject ColorPickedPrefab;
+    private ColorPickerTriangle CP;
+    private bool isPaint = false;
+    private GameObject go;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        audioSource = GetComponents<AudioSource>();
+        dice = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isPaint)
+        {
+            mat.color = CP.TheColor;
+        }
+    }
+
+    public void EditColor()
+    {
+        if (isPaint)
+        {
+            StopPaint();
+        }
+        else
+        {
+            StartPaint();
+        }
+    }
+
+
+    private void StartPaint()
+    {
+        go = (GameObject)Instantiate(ColorPickedPrefab, transform.position + Vector3.up * 1.4f, Quaternion.identity);
+        go.transform.localScale = Vector3.one * 1.5f;
+        go.transform.LookAt(Vector3.up*1000000);
+        if(transform.position.x<0) go.transform.position += (Vector3.right * 2);
+        else go.transform.position -= (Vector3.right * 2);
+        go.transform.parent = transform;
+        dice.isKinematic = true;
+        CP = go.GetComponent<ColorPickerTriangle>();
+        CP.SetNewColor(mat.color);
+        isPaint = true;
+    }
+
+    private void StopPaint()
+    {
+        dice.isKinematic = false;
+        Destroy(go);
+        isPaint = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        dice = GetComponent<Rigidbody>();
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            dice.AddForce(contact.normal * 5);
+        }
+        if (collision.gameObject.name=="Terrain" && collision.relativeVelocity.magnitude > .1f)
+        {
+            audioSource[count].Play();
+            count = (count + 1) % 2;
+        }
+    }
+}
--- Jebudo_Dice/Assets/Scripts/GameManager.cs
+++ Jebudo_Dice/Assets/Scripts/GameManager.cs
@@ -5,18 +5,106 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject dice;
-
+    List<GameObject> diceList = new List<GameObject>();
+    List<Rigidbody> rbList = new List<Rigidbody>();
+    bool selected = false;
+    Dice selectedDiceforPainting;
+    float time=0;
+    bool editing = false;
+    public GameObject light;
     void Start()
     {
-
+        
         if (!Input.gyro.enabled)
         {
             Input.gyro.enabled = true;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            SpawnDice();
         }
     }
 
     void Update()
     {
-        dice.transform.rotation = Input.gyro.attitude;
+        if ((Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))&&!selected)
+        {
+            Ray ray;
+            if (Input.touchCount == 1 && Input.GetTouch(0).phase == 0) ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            else ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100f);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null && notAButton(hit.point))
+                {
+
+                    GameObject touchedObject = hit.transform.gameObject;
+                    if (touchedObject.name == "Terrain")
+                    {
+
+                        foreach (Rigidbody dice in rbList)
+                        {
+                            dice.AddForce(new Vector3(Random.Range(-1, 1), Random.Range(2, 5), Random.Range(-1.5f, 1.5f)) * 200);
+                            dice.AddTorque((transform.up * Random.Range(-2, 2) + transform.forward * Random.Range(-2, 2) + transform.right * Random.Range(-2, 2)) * 150);
+                        }
+                    }
+                    else if (diceList.Contains(touchedObject))
+                    {
+                        selected = true;
+                        selectedDiceforPainting = touchedObject.GetComponent<Dice>();
+                        
+                    }
+                }
+            }
+        }
+        else if ((Input.touchCount == 1 && !(Input.GetTouch(0).phase == TouchPhase.Ended) || !Input.GetMouseButtonUp(0)) && selected)
+        {
+            time += Time.deltaTime;
+            if (time > 1)
+            {
+                selectedDiceforPainting.EditColor();
+                time = 0;
+                editing = true;
+            }
+        }
+        else if ((Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0)) && selected)
+        {
+            time = 0;
+            selected = false;
+            if (!editing)
+            {
+                Rigidbody tmprb = selectedDiceforPainting.gameObject.GetComponent<Rigidbody>();
+                tmprb.AddForce(new Vector3(Random.Range(-1, 1), Random.Range(2, 5), Random.Range(-1.5f, 1.5f)) * 200);
+                tmprb.AddTorque((transform.up * Random.Range(-2, 2) + transform.forward * Random.Range(-2, 2) + transform.right * Random.Range(-2, 2)) * 150);
+            }
+            editing = false;
+        }
+        if(Input.gyro.userAcceleration.magnitude>.001f) light.transform.rotation = Input.gyro.attitude;
+    }
+    public void SpawnDice()
+    {
+        if (diceList.Count < 5)
+        {
+            GameObject newDice = Instantiate(dice);
+            newDice.transform.position = new Vector3(Random.Range(-1, 1), Random.Range(1, 5), Random.Range(-1.5f, 1.5f));
+            diceList.Add(newDice);
+            rbList.Add(newDice.GetComponent<Rigidbody>());
+        }
+    }
+    public void DestroyDice()
+    {
+        if (diceList.Count > 0)
+        {
+            GameObject oldDice = diceList.ToArray()[0];
+            diceList.Remove(oldDice);
+            rbList.Remove(oldDice.GetComponent<Rigidbody>());
+            Destroy(oldDice);
+        }
+    }
+    bool notAButton(Vector3 point)
+    {
+        Debug.Log(point);
+        return ((point.x<2 && point.x>-2)||((point.x < -2 || point.x > 2) && point.z>-.9f));
     }
 }
