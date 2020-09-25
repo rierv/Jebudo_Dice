e9fd192718d2656135610cebab41d6695b647596
riccardo <r.iervolino93@gmail.com>
2020-09-25 05:32:44 +0200

    first dice

---
+++ Jebudo_Dice/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject dice;
+
+    void Start()
+    {
+
+        if (!Input.gyro.enabled)
+        {
+            Input.gyro.enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        dice.transform.rotation = Input.gyro.attitude;
+    }
+}
